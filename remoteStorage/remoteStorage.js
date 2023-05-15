ba35cbe4b815c8492588b08efe9eb4a6d9b141bc
Matthias Plank <matthias.plank@gmx.at>
2023-05-15 20:40:06 +0200

    contact-htmlTemplate, remoteStorage structure

---
+++ remoteStorage/remoteStorage.js
@@ -0,0 +1,21 @@
+const STORAGE_TOKEN = '3FR51KSPUMNX6503O7UTC7XKJ6D5AXANKHMJOHKR';
+const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+
+/*
+********** KEYS ********** 
+* contacts
+*
+*
+************************** 
+*/
+
+async function setItem(key, value) {
+    const payload = { key, value, token: STORAGE_TOKEN };
+    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
+    .then(res => res.json());
+}
+
+async function getItem(key) {
+    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+    return fetch(url).then(res => res.json());
+}
