92fccd48bb907d28e2a51c0a18aff44d0863228f
MustafaSoycan <mustafa.soycan@outlook.com>
2023-05-15 20:10:58 +0200

    drag and drope update

--- board/board.js
+++ board/board.js
@@ -5,7 +5,7 @@ let todos = [
         'description': 'lorem asdfgsatf sadfsadfsaf asfsadfsad asfasfsaf ',
         'kanban': 'to-do'
     },
-
+    
     {
         'id': 1,
         'title': 'Kochen',
@@ -31,30 +31,53 @@ let todos = [
 
 let currentDraggedElement;
 
-function updateHTML(){
+function updateHTML() {
+    showToDoBoard();
+    showInProgressBoard();
+    showAwaitingFeedbackBoard();
+    showDoneBoard();
+}
 
+function startDragging(id) {
+    currentDraggedElement = id;
+}
+
+function generateTodoHTML(element) {
+    return `
+    <div draggable="true" ondragstart="startDragging(${element['id']})" class="todo">
+    <h3> ${element['title']} </h3>
+    ${element['description']} 
+    </div>`;
+}
+
+function showToDoBoard() {
     let toDo = todos.filter(t => t['kanban'] == 'to-do');
     document.getElementById('to-do').innerHTML = '';
     for (let index = 0; index < toDo.length; index++) {
         const element = toDo[index];
         document.getElementById('to-do').innerHTML += generateTodoHTML(element);
     }
+}
 
+function showInProgressBoard() {
     let inProgress = todos.filter(t => t['kanban'] == 'in-progress');
     document.getElementById('in-progress').innerHTML = '';
     for (let index = 0; index < inProgress.length; index++) {
         const element = inProgress[index];
         document.getElementById('in-progress').innerHTML += generateTodoHTML(element);
     }
+}
 
+function showAwaitingFeedbackBoard(){
     let awaitingFeedback = todos.filter(t => t['kanban'] == 'awaiting-feedback');
     document.getElementById('awaiting-feedback').innerHTML = '';
     for (let index = 0; index < awaitingFeedback.length; index++) {
         const element = awaitingFeedback[index];
         document.getElementById('awaiting-feedback').innerHTML += generateTodoHTML(element);
     }
+}
 
-
+function showDoneBoard(){
     let done = todos.filter(t => t['kanban'] == 'done');
     document.getElementById('done').innerHTML = '';
     for (let index = 0; index < done.length; index++) {
@@ -63,24 +86,19 @@ function updateHTML(){
     }
 }
 
-function startDragging(id){
-    currentDraggedElement = id;
-    }
-    
-
-function generateTodoHTML(element){
-    return `
-    <div draggable="true" ondragstart="startDragging(${element['id']})" class="todo">
-    <h3> ${element['title']} </h3>
-    ${element['description']} 
-    </div>`;
-}
-
 function allowDrop(ev) {
     ev.preventDefault();
-  }
+}
 
-  function moveTo(kanban){
+function moveTo(kanban) {
     todos[currentDraggedElement]['kanban'] = kanban; // Z.B. Todo mit id 1: Das Feld 'category' ändert sich zu zb 'closed'
     updateHTML();
-  }
+}
+
+function highlight(id) {
+    document.getElementById(id).classList.add('drag-area-highlight');
+}
+
+function removeHighlight(id) {
+    document.getElementById(id).classList.remove('drag-area-highlight');
+}
