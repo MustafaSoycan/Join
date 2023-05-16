c44546414cf0e05d22f419fa325f0f49d0bf1b8f
MustafaSoycan <mustafa.soycan@outlook.com>
2023-05-16 17:04:22 +0200

    board task div updated

--- board/board.js
+++ board/board.js
@@ -3,13 +3,17 @@ let todos = [
         'id': 0,
         'title': 'Putzen',
         'description': 'lorem asdfgsatf sadfsadfsaf asfsadfsad asfasfsaf ',
+        'category': 'Design',
+        'priority': 'Urgent',
         'kanban': 'to-do'
     },
-    
+
     {
         'id': 1,
         'title': 'Kochen',
         'description': 'lorem asdfgsatf sadfsadfsaf asfsadfsad asfasfsaf ',
+        'category': 'Backoffice',
+        'priority': 'Medium',
         'kanban': 'in-progress'
     },
 
@@ -17,6 +21,8 @@ let todos = [
         'id': 2,
         'title': 'Essen',
         'description': 'lorem asdfgsatf sadfsadfsaf asfsadfsad asfasfsaf ',
+        'category': 'Sales',
+        'priority': 'Low',
         'kanban': 'awaiting-feedback'
     },
 
@@ -24,6 +30,8 @@ let todos = [
         'id': 3,
         'title': 'Sport',
         'description': 'lorem asdfgsatf sadfsadfsaf asfsadfsad asfasfsaf ',
+        'category': 'Marketing',
+        'priority': 'Urgent',
         'kanban': 'done'
     },
 ];
@@ -38,6 +46,8 @@ function updateHTML() {
     showDoneBoard();
 }
 
+
+
 function startDragging(id) {
     currentDraggedElement = id;
 }
@@ -45,10 +55,28 @@ function startDragging(id) {
 function generateTodoHTML(element) {
     return `
     <div draggable="true" ondragstart="startDragging(${element['id']})" class="todo">
-    <h3> ${element['title']} </h3>
+
+    <div id="${element['category']}" class="category">
+    ${element['category']} 
+    </div>
+
+    <div class="title">
+    <b>${element['title']} </b>
+    </div>
+
+    <div class="description">
     ${element['description']} 
+    </div>
+
+    <div id="${element['priority']}" class="priority">
+    ${element['priority']} 
+    </div>
+
     </div>`;
+
 }
+
+
 
 function showToDoBoard() {
     let toDo = todos.filter(t => t['kanban'] == 'to-do');
@@ -56,6 +84,8 @@ function showToDoBoard() {
     for (let index = 0; index < toDo.length; index++) {
         const element = toDo[index];
         document.getElementById('to-do').innerHTML += generateTodoHTML(element);
+        checkCategory(element);
+        checkPriority(element);
     }
 }
 
@@ -65,26 +95,66 @@ function showInProgressBoard() {
     for (let index = 0; index < inProgress.length; index++) {
         const element = inProgress[index];
         document.getElementById('in-progress').innerHTML += generateTodoHTML(element);
+        checkCategory(element);
+        checkPriority(element);
     }
 }
 
-function showAwaitingFeedbackBoard(){
+function showAwaitingFeedbackBoard() {
     let awaitingFeedback = todos.filter(t => t['kanban'] == 'awaiting-feedback');
     document.getElementById('awaiting-feedback').innerHTML = '';
     for (let index = 0; index < awaitingFeedback.length; index++) {
         const element = awaitingFeedback[index];
         document.getElementById('awaiting-feedback').innerHTML += generateTodoHTML(element);
+        checkCategory(element);
+        checkPriority(element);
     }
 }
 
-function showDoneBoard(){
+function showDoneBoard() {
     let done = todos.filter(t => t['kanban'] == 'done');
     document.getElementById('done').innerHTML = '';
     for (let index = 0; index < done.length; index++) {
         const element = done[index];
         document.getElementById('done').innerHTML += generateTodoHTML(element);
+        checkCategory(element);
+        checkPriority(element);
     }
 }
+
+
+function checkPriority(element) {
+    if (element['priority'] == 'Urgent') {
+        document.getElementById('Urgent').innerHTML = `<img src="../img/priority-urgent.png">`;
+    }
+
+    if (element['priority'] == 'Medium') {
+        document.getElementById('Medium').innerHTML = `<img src="../img/priority-medium.png">`;
+    }
+
+    if (element['priority'] == 'Low') {
+        document.getElementById('Low').innerHTML = `<img src="../img/priority-low.png">`;
+    }
+}
+
+function checkCategory(element) {
+    if (element['category'] == 'Design') {
+        document.getElementById('Design').classList.add('design');
+    }
+
+    if (element['category'] == 'Backoffice') {
+        document.getElementById('Backoffice').classList.add('backoffice');
+    }
+
+    if (element['category'] == 'Sales') {
+        document.getElementById('Sales').classList.add('sales');
+    }
+
+    if (element['category'] == 'Marketing') {
+        document.getElementById('Marketing').classList.add('marketing');
+    }
+}
+
 
 function allowDrop(ev) {
     ev.preventDefault();
