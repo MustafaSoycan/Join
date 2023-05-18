e9acafff331737c0da69b0cceb6a194db0f9ecd8
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-05-18 18:47:18 +0200

    add Sign-In function

--- script.js
+++ script.js
@@ -10,21 +10,21 @@ function openPage(page) {
     }
 }
 
-function loadSignUp(){
+function loadSignUp() {
     let loginscreen = document.getElementById('login-screen');
     loginscreen.style.display = 'none';
     let signupscreen = document.getElementById('sign-up-screen');
     signupscreen.style.display = 'flex';
 }
 
-function forgotPW(){
+function forgotPW() {
     let loginscreen = document.getElementById('login-screen');
     loginscreen.style.display = 'none';
     let forgotPW = document.getElementById('forgotPW');
     forgotPW.style.display = 'flex';
 }
 
-function back(){
+function back() {
     let loginscreen = document.getElementById('login-screen');
     loginscreen.style.display = 'flex';
     let signupscreen = document.getElementById('sign-up-screen');
@@ -32,3 +32,45 @@ function back(){
     let forgotPW = document.getElementById('forgotPW');
     forgotPW.style.display = 'none';
 }
+
+
+async function init(){
+    loadUser();
+}
+
+async function loadUser(){
+    try {
+        user = JSON.parse(await getItem('user'));
+    } catch(e){
+        console.error('Loading error:', e);
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let user = [];
+    let nameInput = document.getElementById('name');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let registerBtn = document.getElementById('registerBtn');
+
+    async function register() {
+        registerBtn.disabled = true;
+        user.push({
+            name: nameInput.value,
+            email: email.value,
+            password: password.value,
+        });
+        await setItem('user', JSON.stringify(user));
+        resetForm();
+    }
+
+    function resetForm() {
+        nameInput.value = '';
+        email.value = '';
+        password.value = '';
+        registerBtn.disabled = false;
+    }
+
+    registerBtn.addEventListener('click', register);
+});
