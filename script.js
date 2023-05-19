246f3cd597ee67d07c7a3a0a6fab0f8bec4b9f41
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-05-19 13:45:55 +0200

    add animation for layout

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ function openPage(page) {
         back();
     } else if (page === "forgotPW") {
         forgotPW();
+    }else if (page === "backChange") {
+        backChange();
     }
 }
 
@@ -33,8 +35,44 @@ function back() {
     forgotPW.style.display = 'none';
 }
 
-let user = [];
 
+function  backChange(){
+    let forgotPW = document.getElementById('forgotPW');
+    forgotPW.style.display = 'flex';
+    let changePW = document.getElementById('changePW');
+    changePW.style.display = 'none';
+
+}
+function resetPW(){
+    let animationInfo = document.getElementById('animation-info');
+    animationInfo .style.display = 'flex';
+    let forgotPW = document.getElementById('forgotPW');
+    forgotPW.style.display = 'none';
+    let loginscreen = document.getElementById('login-screen');
+    loginscreen.style.display = 'none';
+    let changePW = document.getElementById('changePW');
+    changePW.style.display = 'flex';
+
+    animationInfo.addEventListener('animationend', function() {
+        animationInfo.style.display = 'none';
+      }, { once: true });
+}
+
+function checkPW(){
+    let animationReset = document.getElementById('animation-reset');
+    animationReset .style.display = 'flex';
+    let changePW = document.getElementById('changePW');
+    changePW.style.display ='none';
+    let loginscreen = document.getElementById('login-screen');
+    loginscreen.style.display = 'flex';
+
+    animationReset.addEventListener('animationend', function() {
+        animationReset.style.display = 'none';
+      }, { once: true });
+}
+
+
+let user = [];
 
 async function init() {
     loadUser();
@@ -65,6 +103,7 @@ document.addEventListener('DOMContentLoaded', function () {
         await setItem('user', JSON.stringify(user));
         resetForm();
         loadUser();
+        back();
     }
 
 
