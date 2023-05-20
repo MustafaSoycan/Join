aeb53f3a25a7bd56695badae5e802fe7f3ea8ddd
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-05-20 11:32:01 +0200

    add if else statement`s

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function openPage(page) {
-    if (page === "summary") {
+    if (page === "log-in") {
+        checkLogin()
+    } else  if (page === "summary") {
         window.location.href = "./summary/summary.html";
     } else if (page === "sign-up") {
         loadSignUp();
@@ -11,6 +13,31 @@ function openPage(page) {
         backChange();
     }
 }
+
+function checkLogin() {
+    let emailInput = document.getElementById('log-in-email');
+    let passwordInput = document.getElementById('log-in-pw');
+  
+    let email = emailInput.value;
+    let password = passwordInput.value;
+  
+    let foundUser = user.find(function (user) {
+      return user.email === email && user.password === password;
+    });
+  
+    if (foundUser) {
+        document.getElementById('current-user').textContent = '';
+        document.getElementById('current-user').textContent = 'Angemeldet ist der Benutzer: ' + foundUser.name;
+        console.log('Name des Benutzers:', foundUser.name);
+        emailInput.value = '';
+        passwordInput.value = '';
+        window.location.href = "./summary/summary.html";
+
+    } else {
+      alert('Die E-Mail-Adresse oder das Passwort ist ungültig.');
+    }
+  }
+  
 
 function loadSignUp() {
     let loginscreen = document.getElementById('login-screen');
@@ -43,33 +70,64 @@ function  backChange(){
     changePW.style.display = 'none';
 
 }
-function resetPW(){
-    let animationInfo = document.getElementById('animation-info');
-    animationInfo .style.display = 'flex';
-    let forgotPW = document.getElementById('forgotPW');
-    forgotPW.style.display = 'none';
-    let loginscreen = document.getElementById('login-screen');
-    loginscreen.style.display = 'none';
-    let changePW = document.getElementById('changePW');
-    changePW.style.display = 'flex';
-
-    animationInfo.addEventListener('animationend', function() {
+function resetPW() {
+    let emailInput = document.getElementById('email-forgot');
+    let emailValue = emailInput.value;
+  
+    if (emailValue !== '' && isValidEmail(emailValue)) {
+      let animationInfo = document.getElementById('animation-info');
+      animationInfo.style.display = 'flex';
+  
+      let forgotPW = document.getElementById('forgotPW');
+      forgotPW.style.display = 'none';
+  
+      let loginscreen = document.getElementById('login-screen');
+      loginscreen.style.display = 'none';
+  
+      let changePW = document.getElementById('changePW');
+      changePW.style.display = 'flex';
+  
+      animationInfo.addEventListener('animationend', function() {
         animationInfo.style.display = 'none';
       }, { once: true });
-}
+    } else {
+        alert('bitte eine gültige email eingeben')
+    }
+  }
+  
+  function isValidEmail(email) {
+    return email.includes('@');
+  }
+  
+  
 
-function checkPW(){
-    let animationReset = document.getElementById('animation-reset');
-    animationReset .style.display = 'flex';
-    let changePW = document.getElementById('changePW');
-    changePW.style.display ='none';
-    let loginscreen = document.getElementById('login-screen');
-    loginscreen.style.display = 'flex';
-
-    animationReset.addEventListener('animationend', function() {
+  function checkPW() {
+    let newPasswordInput = document.getElementById('new-password');
+    let confirmPasswordInput = document.getElementById('confirm-password');
+  
+    let newPassword = newPasswordInput.value;
+    let confirmPassword = confirmPasswordInput.value;
+  
+    if (newPassword !== '' && confirmPassword !== '' && newPassword === confirmPassword) {
+      let animationReset = document.getElementById('animation-reset');
+      animationReset.style.display = 'flex';
+  
+      let changePW = document.getElementById('changePW');
+      changePW.style.display = 'none';
+  
+      let loginscreen = document.getElementById('login-screen');
+      loginscreen.style.display = 'flex';
+  
+      animationReset.addEventListener('animationend', function() {
         animationReset.style.display = 'none';
       }, { once: true });
-}
+    } else {
+      alert('Bitte geben Sie gültige Passwörter ein.');
+    }
+  }
+  
+  
+  
 
 
 let user = [];
@@ -88,34 +146,64 @@ async function loadUser() {
 
 
 
+
 document.addEventListener('DOMContentLoaded', function () {
-    let nameInput = document.getElementById('name');
-    let email = document.getElementById('email');
-    let password = document.getElementById('password');
-    let registerBtn = document.getElementById('registerBtn');
-    async function register() {
-        registerBtn.disabled = true;
-        user.push({
-            name: nameInput.value,
-            email: email.value,
-            password: password.value,
-        });
-        await setItem('user', JSON.stringify(user));
-        resetForm();
-        loadUser();
-        back();
+  let nameInput = document.getElementById('name');
+  let email = document.getElementById('email');
+  let password = document.getElementById('password');
+  let registerBtn = document.getElementById('registerBtn');
+
+  async function register() {
+    if (nameInput.value === '' || email.value === '' || password.value === '') {
+      alert('Bitte füllen Sie alle Felder aus.');
+      return;
     }
 
-
-    function resetForm() {
-        nameInput.value = '';
-        email.value = '';
-        password.value = '';
-        registerBtn.disabled = false;
+    if (!isValidEmail(email.value)) {
+      alert('Bitte geben Sie eine gültige E-Mail-Adresse ein.');
+      return;
     }
 
+    if (isUserAlreadyRegistered(nameInput.value, email.value)) {
+      alert('Der Name oder die E-Mail-Adresse ist bereits vergeben.');
+      return;
+    }
 
-    registerBtn.addEventListener('click', register);
+    registerBtn.disabled = true;
+
+    user.push({
+      name: nameInput.value,
+      email: email.value,
+      password: password.value,
+    });
+
+    await setItem('user', JSON.stringify(user));
+    resetForm();
+    loadUser();
+    back();
+  }
+
+  function resetForm() {
+    nameInput.value = '';
+    email.value = '';
+    password.value = '';
+    registerBtn.disabled = false;
+  }
+
+  function isValidEmail(email) {
+    return email.includes('@');
+  }
+
+  function isUserAlreadyRegistered(name, email) {
+    for (let i = 0; i < user.length; i++) {
+      if (user[i].name === name || user[i].email === email) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  registerBtn.addEventListener('click', register);
 });
 
 
