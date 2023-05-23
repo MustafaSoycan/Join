1928a0a6b8ea6c62c372d22a2c8f001854220fc6
Matthias Plank <matthias.plank@gmx.at>
2023-05-23 22:01:08 +0200

    summary urgent date

--- summary/summary.js
+++ summary/summary.js
@@ -15,7 +15,9 @@ function setItemValues(){
     document.getElementById("tasksInBoard").innerHTML = tasks.length ; 
     document.getElementById("tasksInProgress").innerHTML = tasks.filter(task => task.kanban == "in-progress").length; 
     document.getElementById("tasksAwaitingFeedback").innerHTML = tasks.filter(task => task.kanban == "awaiting-feedback").length; 
-    document.getElementById("tasksUrgent").innerHTML = tasks.filter(task => task.priority == "urgent").length;  ; 
+    document.getElementById("tasksUrgent").innerHTML = tasks.filter(task => task.priority == "urgent").length;  
+    document.getElementById("taskUrgentDate").innerHTML = getNextUrgentDate();  
+ 
     document.getElementById("tasksToDo").innerHTML = tasks.filter(task => task.kanban == "to-do").length; 
     document.getElementById("tasksDone").innerHTML = tasks.filter(task => task.kanban == "done").length; 
 }
@@ -39,4 +41,23 @@ async function getTasksFromLocalStorage(){ /* ACHTUNG: NUR TESTDATEN*/
           tasks =  [];
         }
    
+}
+
+function getNextUrgentDate(){
+
+  let urgent = tasks.filter(task => task.priority == "urgent"); 
+  console.log(urgent);
+  if ( urgent.length > 0 ) {
+    urgent = urgent.sort (function(a,b){
+      // Turn your strings into dates, and then subtract them
+      // to get a value that is either negative, positive, or zero.
+      return new Date(a.dueDate) - new Date(b.dueDate);
+    });
+    return urgent[0].dueDate; 
+  }
+  else {
+    return "No urgent task"; 
+  }
+
+ 
 }
