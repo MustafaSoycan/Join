2c74a3e179cb32a7ece7cffb1c3334c48588b23c
MustafaSoycan <mustafa.soycan@outlook.com>
2023-10-03 13:13:05 +0200

    added highlight to navigation link, edit-task without scrollbar

--- templates/include.js
+++ templates/include.js
@@ -3,51 +3,76 @@
  * @returns Git das den inklidierten HTML Code zurück
  */
 function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("w3-include-html");
-      if (file) {
-        /* Make an HTTP request using the attribute value as the file name: */
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /* Remove the attribute, and call this function once more: */
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-          }
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+          if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
         }
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /* Exit the function: */
-        return;
       }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
     }
   }
-  /**
-   * Funktion zum Ausloggen aus dem JOIN System. ‚
-   */
-  function logOut() {
-    localStorage.removeItem('username');
-  
-    let logOut = document.getElementById('logOut');
-  
-    if (logOut.classList.contains('d-none')) {
-      logOut.classList.remove('d-none');
-    } else {
-      logOut.classList.add('d-none');
+
+  highlightLinks();
+}
+
+
+
+
+function highlightLinks() {
+  // Bestimmen Sie die aktuelle Seite durch Abrufen des Pfads der URL
+  let path = window.location.pathname;
+  path = path.split("/");
+
+  // Wählen Sie alle Links im Navigationsmenü aus
+  let links = document.querySelectorAll(".summary, .board, .addTask, .contacts, .datenschutz");
+
+  // Überprüfen Sie jeden Link
+  links.forEach((link) => {
+    // Wenn der Pfad des Links zur aktuellen Seite führt
+    if (link.getAttribute("href").includes(path[2])) {
+      // Dann fügen Sie die "active" Klasse zu diesem Link hinzu
+      link.classList.add("active-link");
     }
+  });
+}
+
+
+/**
+ * Funktion zum Ausloggen aus dem JOIN System. ‚
+ */
+function logOut() {
+  localStorage.removeItem('username');
+
+  let logOut = document.getElementById('logOut');
+
+  if (logOut.classList.contains('d-none')) {
+    logOut.classList.remove('d-none');
+  } else {
+    logOut.classList.add('d-none');
   }
+}
 
 /**
  * Öffnet das mobile Menü
  */
-function openMenu(){
+function openMenu() {
   let mobileMenu = document.getElementById('mobileMenuPopup');
   if (mobileMenu.classList.contains('d-none')) {
     mobileMenu.classList.remove('d-none');
@@ -56,30 +81,4 @@ function openMenu(){
   }
 }
 
-window.addEventListener("DOMContentLoaded", function() {
-  var activePage = localStorage.getItem("activePage");
-  if (activePage) {
-      var link = document.querySelector('a[data-page="' + activePage + '"]');
-      if (link) {
-          link.classList.add("active");
-      }
-  }
-});
 
-function changeActivePage(event, pageName) {
-  event.preventDefault(); // Verhindert das Laden der neuen Seite
-
-  var clickedElement = event.target;
-  
-  // Entferne die Klasse "active" von allen Links
-  var links = document.getElementsByTagName("a");
-  for (var i = 0; i < links.length; i++) {
-      links[i].classList.remove("active");
-  }
-
-  // Füge die Klasse "active" zum geklickten Link hinzu
-  clickedElement.classList.add("active");
-
-  // Speichere den Zustand der aktiven Seite im Web Storage
-  localStorage.setItem("activePage", pageName);
-}
